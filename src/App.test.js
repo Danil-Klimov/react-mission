69f05839b7a59dd791ca055c815f8af9b360af33
Danil-Klimov <doink3690@gmail.com>
2019-05-11 09:10:58 +0300

    update tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,46 +1,45 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
-import App from './App';
-import Table from './components/Table';
+import TableContainer from "./components/TableContainer";
 
 describe('App', () => {
   describe('initial render', () => {
-    const app = mount(<App initialWidth={5} initialHeight={5} />);
+    const tableContainer = mount(<TableContainer initialWidth={5} initialHeight={5} />);
 
     it('initialHeight render table rows', () => {
-      expect(app.find('.table tr').getElements().length).toEqual(app.prop('initialHeight'));
+      expect(tableContainer.find('.table tr').getElements().length).toEqual(tableContainer.prop('initialHeight'));
     });
 
     it('initialWidth render table cols', () => {
-      expect(app.find('table tr:first-child td').getElements().length).toEqual(app.prop('initialWidth'));
+      expect(tableContainer.find('table tr:first-child td').getElements().length).toEqual(tableContainer.prop('initialWidth'));
     });
   });
 
   describe('add buttons', () => {
-    const app = mount(<App initialWidth={4} initialHeight={4} />);
+    const tableContainer = mount(<TableContainer initialWidth={4} initialHeight={4} />);
 
     it('add row', () => {
-      app.find('button.table__button_add-row').simulate('click');
-      expect(app.find('.table tr').getElements().length).toEqual(app.state().rows.rowsArray.length);
+      tableContainer.find('button.table__button_add-row').simulate('click');
+      expect(tableContainer.find('.table tr').getElements().length).toEqual(tableContainer.state().rows.length);
     });
 
     it('add col', () => {
-      app.find('button.table__button_add-col').simulate('click');
-      expect(app.find('table tr:first-child td').getElements().length).toEqual(app.state().cells.cellsArray.length);
+      tableContainer.find('button.table__button_add-col').simulate('click');
+      expect(tableContainer.find('table tr:first-child td').getElements().length).toEqual(tableContainer.state().cells.length);
     });
   });
 
   describe('del buttons', () => {
-    const app = mount(<App initialWidth={4} initialHeight={4} />);
+    const tableContainer = mount(<TableContainer initialWidth={4} initialHeight={4} />);
 
     it('del row', () => {
-      app.find('button.table__button_del-row').simulate('click');
-      expect(app.find('.table tr').getElements().length).toEqual(app.state().rows.rowsArray.length);
+      tableContainer.find('button.table__button_del-row').simulate('click');
+      expect(tableContainer.find('.table tr').getElements().length).toEqual(tableContainer.state().rows.length);
     });
 
     it('del col', () => {
-      app.find('button.table__button_del-col').simulate('click');
-      expect(app.find('table tr:first-child td').getElements().length).toEqual(app.state().cells.cellsArray.length);
+      tableContainer.find('button.table__button_del-col').simulate('click');
+      expect(tableContainer.find('table tr:first-child td').getElements().length).toEqual(tableContainer.state().cells.length);
     });
   });
 });
